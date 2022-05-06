3701c02b778c5dc6a871eff6e2315dcc6ed0c28a
SkyZen <devpozdeevk@gmail.com>
2022-05-06 11:47:00 +0400

    3

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,11 +2,26 @@ let header2 = document.querySelector(".header2");
 let header = document.querySelector(".header");
 
 
+let minOpenBtn = document.querySelector(".menu_btn");
+let minCloseBtn = document.querySelector('.close_minMenu_btn');
+let minMenu = document.querySelector('.min_menu');
+
+
 const posTop = document.querySelector("#catalog").getBoundingClientRect().top;
 
 
 
+minOpenBtn.addEventListener('click', function(e) {
+    if (minMenu.style.display != "block") { 
+        minMenu.style.display = "block"; //Показываем элемент
+    }
+});
 
+minCloseBtn.addEventListener('click', function(e) {
+    if (minMenu.style.display = "block") { 
+        minMenu.style.display = "none"; //Показываем элемент
+    }
+});
 
 
 document.addEventListener('scroll', function() {
